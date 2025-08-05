505717de5bdb8c5730ff34fef18d9e147fcd61ea
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-08-05 23:46:19 +0530

    document analyser code updated

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,20 +1,33 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
-
+from prompt.prompt_library import PROMPT_REGISTRY # type: ignore
 
 class DocumentAnalyzer:
     """
-    Main class for document analysis.
-    Loads models, processes documents, and handles analysis logic.
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
     """
-
     def __init__(self):
-        pass
-
-    def analyze_metadata(self):
-        pass
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader=ModelLoader()
+            self.llm=self.loader.load_llm()
+            
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+            
+            self.prompt = PROMPT_REGISTRY["document_analysis"]
+            
+            self.log.info("DocumentAnalyzer initialized successfully")
+            
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
