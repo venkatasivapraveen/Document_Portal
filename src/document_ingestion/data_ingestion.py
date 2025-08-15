27081bb0198ecb19a8745ace2c4227556b29e5f3
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-08-15 19:10:30 +0530

    new data_ingestion.py updated

---
+++ src/document_ingestion/data_ingestion.py
@@ -0,0 +1,69 @@
+
+
+
+
+
+class FaissManager:
+    def __init__(self):
+        pass
+
+    def _exists(self):
+        pass
+
+    @staticmethod
+    def _fingerprint():
+        pass
+
+    def _save_meta(self):
+        pass
+
+    def load_or_create(self):
+        pass
+
+
+
+class DocHandler:
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
+
+
+class DocumentComparator:
+    def __init__(self):
+        pass
+
+    def save_uploaded_files(self):
+        pass
+
+    def read_pdf(self):
+        pass
+
+    def combine_documents(self):
+        pass
+
+
+    def clean_old_sessions(self):
+        pass
+
+
+
+
+class ChatIngestor:
+    def __init__(self):
+        pass
+
+
+    def _resolve_dir(self):
+        pass
+
+    def _split(self):
+        pass
+
+    def built_retriver(self):
+        pass
+ 
