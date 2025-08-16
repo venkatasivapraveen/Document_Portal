2ed2e4b63881952e9ae5a37c229f4d1d6de7a346
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-08-16 23:47:15 +0530

    main was updated

---
+++ api/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, UploadFile, File, form, HTTPException, Request
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+from typing import List, Optional, Any, Dict
